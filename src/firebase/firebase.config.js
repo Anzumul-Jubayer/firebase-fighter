d39e5d99502d16651a7f63e187759e6428f2b952
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-10-17 00:36:07 +0600

    'auth-secure

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_kRthgl8zoxoULrSN0yCiC3mT5EgGW7A",
-  authDomain: "fir-fighter-cbdf1.firebaseapp.com",
-  projectId: "fir-fighter-cbdf1",
-  storageBucket: "fir-fighter-cbdf1.firebasestorage.app",
-  messagingSenderId: "925270847277",
-  appId: "1:925270847277:web:7f105b0efa87c34ff82da4"
+   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+
 };
 
 // Initialize Firebase
